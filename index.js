9557d5d983b134282d35582fe84f815923aa566c
Lior Subotnick <theawesomepandapig@gmail.com>
2021-01-11 23:06:19 -0800

    Site Update

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
+var stockPrice = 0;
+
+fetch('https://fcc-stockchecker.glitch.me//api/stock-prices?ticker1=NVDA').then(function(response) {
+    return response.json();
+}).then(function(data) {
+    stockPrice =  data.stockData.price;
+    document.getElementById("price").innerHTML = "$" + stockPrice;
+    propogate();
+}).catch(function() {
+    console.log("Data failed to load.");
+});
+
+document.addEventListener("DOMContentLoaded", function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById("made-by-link").style.borderColor = color;
+    document.getElementById("made-by-link").style.color = color;
+});
+
 function propogate() {
-    $.getJSON('https://fcc-stockchecker.glitch.me//api/stock-prices?ticker1=NVDA', function(data) {
-    var stockPrice = data.stockData.price;
-    var cardPrice = 499;
-    document.getElementById("price").innerHTML += stockPrice;
+    var cardPrice = 0;
+    var card = "";
+    document.getElementById('dollar-amt-span').innerHTML = "$";
+    document.getElementById('dollar-amt-span').innerHTML = "$";
+    document.getElementById("3060Ti-img").style.display = "none";
+    document.getElementById("3070-img").style.display = "none";
+    document.getElementById("3080-img").style.display = "none";
+    document.getElementById("3090-img").style.display = "none";
+    if (document.getElementById("card-select").value == "3060Ti") {
+        cardPrice = 399.99;
+        card = "RTX 3060 Ti";
+        document.getElementById("card-name").innerHTML = card;
+        document.getElementById("3060Ti-img").style.display = "block";
+    } else if (document.getElementById("card-select").value == "3070") {
+        cardPrice = 499.99;
+        card = "RTX 3070";
+        document.getElementById("card-name").innerHTML = card;
+        document.getElementById("3070-img").style.display = "block";
+    } else if (document.getElementById("card-select").value == "3080") {
+        cardPrice = 699.99;
+        card = "RTX 3080";
+        document.getElementById("card-name").innerHTML = card;
+        document.getElementById("3080-img").style.display = "block";
+    } else {
+        cardPrice = 1499.99
+        card = "RTX 3090";
+        document.getElementById("card-name").innerHTML = card;
+        document.getElementById("3090-img").style.display = "block";
+    }
+    document.getElementById("price-card").innerHTML = "$" + cardPrice;
     var diff = stockPrice - cardPrice;
     diff = diff.toFixed(2);
     if (diff > 0) {
@@ -21,17 +69,7 @@ function propogate() {
         document.getElementById('price').style.color = "#000000";
         document.getElementById('price-card').style.color = "#000000";
     }
+    document.getElementById("selected-card").innerHTML = card;
     document.getElementById('price').style.display = "block";
     document.getElementById('price-card').style.display = "block";
-    });
 }
-
-document.addEventListener("DOMContentLoaded", function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    document.getElementById("made-by-link").style.borderColor = color;
-    document.getElementById("made-by-link").style.color = color;
-});
